f29006c4dff1fa5033167c37812f3c744db9424b
vlad <caraseli02@gmail.com>
2021-07-24 12:48:45 +0200

    refact: move nav in component

--- src/App.vue
+++ src/App.vue
@@ -1,135 +1,26 @@
 <template>
   <div>
-    <nav class="">
-      <div class="max-w-screen-xl mx-auto px-6 sm:px-10 lg:px-16">
-        <div class="flex items-center justify-between h-16">
-          <div class="flex items-center justify-between">
-            <div></div>
-            <h1 class="text-xl font-bold">Tabs Component</h1>
-            <div class="hidden md:block">
-              <div class="ml-10 flex items-baseline">
-                <router-link
-                  v-for="(link, i) in links"
-                  :key="i"
-                  :to="link.to"
-                  custom
-                  v-slot="{ navigate, href, isExactActive }"
-                >
-                  <a
-                    :href="href"
-                    @click="navigate"
-                    class="px-3 py-2 rounded-md text-sm font-medium"
-                    :class="[
-                      isExactActive
-                        ? 'text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700'
-                        : 'text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700',
-                      i > 0 && 'ml-4',
-                    ]"
-                    >{{ link.text }}</a
-                  >
-                </router-link>
-              </div>
-            </div>
-          </div>
-          <div class="-mr-2 flex md:hidden">
-            <!-- Mobile menu button -->
-            <button
-              class="
-                inline-flex
-                items-center
-                justify-center
-                p-2
-                rounded-md
-                text-gray-400
-                hover:text-white
-                hover:bg-gray-700
-                focus:outline-none
-                focus:bg-gray-700
-                focus:text-white
-              "
-              @click="showMenu = !showMenu"
-            >
-              <!-- Menu open: "hidden", Menu closed: "block" -->
-              <svg
-                class="block h-6 w-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <!-- Menu open: "block", Menu closed: "hidden" -->
-              <svg
-                class="hidden h-6 w-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <!--
-      Mobile menu, toggle classes based on menu state.
-
-      Open: "block", closed: "hidden"
-    -->
-      <div class="md:hidden" :class="showMenu ? 'block' : 'hidden'">
-        <div class="px-2 pt-2 pb-3 sm:px-3">
-          <router-link
-            v-for="(link, i) in links"
-            :key="i"
-            :to="link.to"
-            custom
-            v-slot="{ navigate, href, isExactActive }"
-          >
-            <a
-              :href="href"
-              @click="navigate().then(() => (showMenu = false))"
-              class="block px-3 py-2 rounded-md text-base font-medium"
-              :class="[
-                isExactActive
-                  ? 'text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700'
-                  : 'text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700',
-                i > 0 && 'mt-1',
-              ]"
-              >{{ link.text }}</a
-            >
-          </router-link>
-        </div>
-      </div>
-    </nav>
+    <navBar />
     <div class="max-w-screen-xl mx-auto py-6 sm:px-6 lg:px-8">
-      <router-view />
+      <router-view v-slot="{ Component }">
+      <transition
+        name="slide-right"
+      >
+        <component :is="Component" />
+      </transition>
+    </router-view>
     </div>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-
+import NavBar from "./components/NavBar.vue";
 export default defineComponent({
-  data: () => ({
-    showMenu: false,
-    showProfileMenu: false,
-    links: [
-      { text: "Home", to: "/" },
-      { text: "About", to: "/about" },
-    ],
-  }),
+  components: {
+    NavBar,
+  },
+  watch: {},
 });
 </script>
 
@@ -141,5 +32,18 @@ html {
   background-repeat: no-repeat;
   width: 100%;
   height: 100%;
+}
+.slide-right-enter-active {
+  transition: all 0.5s ease-out;
+}
+
+.slide-right-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-right-enter-from,
+.slide-right-leave-to {
+  transform: translateX(-20px);
+  opacity: 0;
 }
 </style>
